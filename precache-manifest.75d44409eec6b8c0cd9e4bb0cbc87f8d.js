912ea78ab48df8203b523b3a9fa0581302e2f628
Martin Santa Cruz <mesantacruz@uc.cl>
2020-05-17 22:10:32 -0400

    Updates

---
+++ precache-manifest.75d44409eec6b8c0cd9e4bb0cbc87f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e93cf8f47c2f81a2cda34e084a5004",
+    "url": "/tarea3/index.html"
+  },
+  {
+    "revision": "84cc64c196f46b284e48",
+    "url": "/tarea3/static/css/main.d25e4d8c.chunk.css"
+  },
+  {
+    "revision": "b1b205725f61153fe582",
+    "url": "/tarea3/static/js/2.39e4e426.chunk.js"
+  },
+  {
+    "revision": "c04130d8b6692f26b622ca7bd7d5f6b5",
+    "url": "/tarea3/static/js/2.39e4e426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84cc64c196f46b284e48",
+    "url": "/tarea3/static/js/main.65c37fc4.chunk.js"
+  },
+  {
+    "revision": "11d2a0f1debdae651222",
+    "url": "/tarea3/static/js/runtime-main.ed6a7d9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tarea3/static/media/logo.5d5d9eef.svg"
+  }
+]);
